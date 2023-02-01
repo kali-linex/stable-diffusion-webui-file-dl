389509dedce90ec48a13fb7e6074537692d0b967
kali-linex <87039059+kali-linex@users.noreply.github.com>
2023-02-01 20:14:37 +0000

    make basic extension

---
+++ scripts/downloader.py
@@ -0,0 +1,21 @@
+import os
+from modules import script_callbacks, paths
+import gradio as gr
+import requests
+
+def on_ui_tabs():
+    with gr.Blocks(analytics_enabled=False) as downloader:
+        url = gr.Textbox("URL")
+        out_file = gr.Textbox("Output file/folder if MEGA, based on models directory")
+        download = gr.Button("Download")
+        done = gr.Text()
+        def download_file(url, out_file, progress=gr.Progress()):
+            progress(0, "Starting download")
+            r = requests.get(url, stream=True, timeout=10000)
+            with open(os.path.join(paths.models_path, out_file), 'wb') as f:
+                for data in progress.tqdm(r.iter_content(1024)):
+                    f.write(data)
+            return "done"
+        download.click(download_file, inputs=[url, out_file], outputs=[done])
+
+script_callbacks.on_ui_tabs(on_ui_tabs)
