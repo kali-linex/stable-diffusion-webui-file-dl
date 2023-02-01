6c80f5cec8ee36e402a0700229e1e817ffc85086
kali-linex <87039059+kali-linex@users.noreply.github.com>
2023-02-01 20:46:13 +0000

    fix

--- install.py
+++ install.py
@@ -2,3 +2,6 @@ import launch
 
 if not launch.is_installed("requests"):
     launch.run_pip("install requests", "requirements for downloading files")
+
+if not launch.is_installed("tqdm"):
+    launch.run_pip("install tqdm", "requirements for downloading files")
--- scripts/downloader.py
+++ scripts/downloader.py
@@ -2,6 +2,7 @@ import os
 from modules import script_callbacks, paths
 import gradio as gr
 import requests
+import tqdm
 
 def on_ui_tabs():
     with gr.Blocks(analytics_enabled=False) as downloader:
@@ -9,11 +10,11 @@ def on_ui_tabs():
         out_file = gr.Textbox("Output file/folder if MEGA, based on models directory")
         download = gr.Button("Download")
         done = gr.Text()
-        def download_file(url, out_file, progress=gr.Progress()):
+        def download_file(url, out_file):
             progress(0, "Starting download")
             r = requests.get(url, stream=True, timeout=10000)
             with open(os.path.join(paths.models_path, out_file), 'wb') as f:
-                for data in progress.tqdm(r.iter_content(1024)):
+                for data in tqdm.tqdm(r.iter_content(1024)):
                     f.write(data)
             return "done"
         download.click(download_file, inputs=[url, out_file], outputs=[done])
