b25e0aa79eea870b0cf30806b4fc75f0c8961d27
kali-linex <87039059+kali-linex@users.noreply.github.com>
2023-02-28 19:20:22 +0000

    change directory

--- install.py
+++ install.py
@@ -1,4 +1,5 @@
 import launch
+import os
 
 if not launch.is_installed("requests"):
     launch.run_pip("install requests", "requirements for downloading files")
@@ -6,4 +7,4 @@ if not launch.is_installed("requests"):
 if not launch.is_installed("tqdm"):
     launch.run_pip("install tqdm", "requirements for downloading files")
 
-launch.run("bash mega_load.bash", "download MEGAcmd")
+launch.run(f"bash {os.path.dirname(os.path.realpath(__file__))}/mega_load.bash", "download MEGAcmd")
